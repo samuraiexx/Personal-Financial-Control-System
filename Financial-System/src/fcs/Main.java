f1c5234a1ac86c11e4863408ade650f976bdde0a
Luciano Barreira <luc.s.barreira@gmail.com>
2019-05-25 22:37:30 -0300

    Refactor main to match login/signup page needs

--- Financial-System/src/fcs/Main.java
+++ Financial-System/src/fcs/Main.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class Main {
-	private static List<Usuario> usuarios;
+	private  List<Usuario> usuarios;
 	
-	public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	public static final String DB_URL = "jdbc:mysql://localhost/fcs";
+	public final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	public final String DB_URL = "jdbc:mysql://localhost/fcs";
 
-	public static final String DB_USER = "username";
-	public static final String PASSWORD = "password";
+	public final String DB_USER = "username";
+	public final String PASSWORD = "password";
 	
-	public static Connection conn;
+	public Connection conn;
 
-	private static String current_user;
+	private String current_user;
 
-	private Main() {
+	public Main() {
 		usuarios = new ArrayList<Usuario>();
 	}
 	
-	public static void setup() {
+	public void Connect() {
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn = DriverManager.getConnection(DB_URL, DB_USER, PASSWORD);
@@ -29,24 +29,23 @@ public final class Main {
 			e.printStackTrace();
 		}
 	}
-
-	public static void main(String[] args) {
-		setup();
-	}
 	
-	public static void registra_usuario(String usr, String pwd) {
+	public boolean registra_usuario(String usr, String pwd) {
 		try {
 			Statement statement = conn.createStatement();
 			int result = statement.executeUpdate("INSERT INTO usuarios(nome,senha) VALUES('"+usr+"','"+pwd+"');");
 			usuarios.add(new Usuario(usr,pwd));
+			return true;
 		} catch (SQLIntegrityConstraintViolationException e) {
 			System.out.println("Usuario ja existe");
+			return false;
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return false;
 		}
 	}
 	
-	public static void login(String usr, String pwd) {
+	public boolean login(String usr, String pwd) {
 		try {
 			Statement statement = conn.createStatement();
 			ResultSet rs = statement.executeQuery("SELECT senha FROM usuarios WHERE nome='"+usr+"';");
@@ -55,11 +54,14 @@ public final class Main {
 			if (pass.equals(pwd)) {
 				System.out.println("Login realizado com sucesso!" + " usuario: "+usr);
 				current_user = usr;
+				return true;
 			} else {
 				System.out.println("Senha incorreta.");
+				return false;
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
+			return false;
 		}
 	}
 
